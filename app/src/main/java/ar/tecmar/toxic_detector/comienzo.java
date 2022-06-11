1eddbc5f74b0de1cb906bef0412a15fbc6fafb2b
unknown <you@example.comelcistdivino@gmail.com>
2022-06-11 18:06:20 -0300

    Se crea la segunda activity para comenzar con la app

--- app/src/main/java/ar/tecmar/toxic_detector/comienzo.java
+++ app/src/main/java/ar/tecmar/toxic_detector/comienzo.java
@@ -1,13 +1,26 @@
 package ar.tecmar.toxic_detector;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class comienzo extends AppCompatActivity {
+    MediaPlayer mp ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comienzo);
+        mp = MediaPlayer.create(this, R.raw.sonido_botono_futiro) ;
+
     }
+    // SE CREA METODO QUE INICIA PAREJA TOXICA
+    public void btnParejaToxica (View v) {
+        mp.start();
+        Intent inicio = new Intent(this,ParejaToxica.class);
+        startActivity(inicio);
+    }
+
 }
